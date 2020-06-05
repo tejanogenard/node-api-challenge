f82a1c44f6fd6ca376c1e005ffd0036c1469bc06
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-05 10:22:10 -0700

    Post and Get endpoint completed for Prodjects

--- data/routes/projectsRouter.js
+++ data/routes/projectsRouter.js
@@ -2,7 +2,24 @@ const express = require('express')
 const projects = require('../helpers/projectModel')
 const router = express.Router()
 
+//Get endpoint for all the projects 
+router.get('/', (req, res) => {
+    projects.get()
+    .then( project => res.status(200).json(project))
+    .catch( err => res.status(500).json({ message :" Projects could not be retrieved", err}) )       
+})
 
+//Post endpoint to create a new project 
+router.post('/', (req, res) => {
+    projects.insert(req.body)
+    .then( project => res.status(200).json(project))
+    .catch( err => res.status(500).json({ message: "Project could not be added", err}))
+})
+
+//Update endpoint to update a current project 
+router.put('/:id', (req, res) => {
+    
+})
 
 
 module.exports = router
