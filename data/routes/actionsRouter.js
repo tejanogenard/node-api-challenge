1389784640408da46c481a30ee4d547309117449
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-05 11:13:15 -0700

    Get by id endpoint for projects completed

--- data/routes/actionsRouter.js
+++ data/routes/actionsRouter.js
@@ -2,6 +2,10 @@ const express = require('express')
 const actions = require('../helpers/actionModel')
 const router = express.Router()
 
+//Get endpoint for all actions 
+
+
+
 
 
 
--- data/routes/projectsRouter.js
+++ data/routes/projectsRouter.js
@@ -5,31 +5,52 @@ const router = express.Router()
 //Get endpoint for all the projects 
 router.get('/', (req, res) => {
     projects.get()
-    .then( project => res.status(200).json(project))
-    .catch( err => res.status(500).json({ message :" Projects could not be retrieved", err}) )       
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({
+            message: " Projects could not be retrieved",
+            err
+        }))
+})
+
+//Get endpoint for a single project 
+router.get('/:id', (req, res) => {
+    projects.get(req.params.id)
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({
+            message: "Prodject could not be retried",
+            err
+        }))
 })
 
 //Post endpoint to create a new project 
 router.post('/', (req, res) => {
     projects.insert(req.body)
-    .then( project => res.status(200).json(project))
-    .catch( err => res.status(500).json({ message: "Project could not be added", err}))
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({
+            message: "Project could not be added",
+            err
+        }))
 })
 
 //Update endpoint to update a current project 
 router.put('/:id', (req, res) => {
     projects.update(req.params.id, req.body)
-    .then(project => res.status(200).json(project))
-    .catch(err => res.status(500).json({ message: "There was an error while retrieving projects.", err }))
-  });
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({
+            message: "There was an error while retrieving projects.",
+            err
+        }))
+});
 
-  //Delete endpoint to delete an existing project 
-  router.delete('/:id', (req, res) => {
-      projects.remove(req.params.id)
-      .then(project => res.status(200).json(project))
-      .catch(err => res.status(500).json({ message: "There was an error deleting projects", err }))
-
-  })
+//Delete endpoint to delete an existing project 
+router.delete('/:id', (req, res) => {
+    projects.remove(req.params.id)
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({
+            message: "There was an error deleting projects",
+            err
+        }))
+})
 
 
 module.exports = router
